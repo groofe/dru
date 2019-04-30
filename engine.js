cc673cda1d85ad18c48ec747a2015ff153e7171c
Patrick Eddy <p@patrickeddy.com>
2019-04-29 20:50:04 -0700

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,7 +1,6 @@
 function run() {
   var bodyElem = document.querySelector('body')
   if (bodyElem) {
-    bodyElem.
     bodyElem.innerHTML = bodyElem.innerHTML
       .split('\n')
       .map(line => `<p class="line">${line}</p>`).join('\n')
